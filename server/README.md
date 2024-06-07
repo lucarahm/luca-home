12b5743c591553cff3c9449565372ef692bc5712
Luca Rahm <lucarahm@web.de>
2024-06-07 20:43:45 +0200

    Getting things ready to deploy the first sketch of the website as POC

---
+++ server/README.md
@@ -0,0 +1,16 @@
+# Setup of the Server
+
+## Steps to follow 
+
+### Firewall
+
+```
+ufw default deny incoming
+ufw allow 22/tcp
+ufw enable
+ufw status
+```
+
+### Deployment of containers for services:
+
+https://github.com/nginx-proxy/acme-companions
--- svelte-frontend/src/routes/+page.svelte
+++ svelte-frontend/src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="p-4 text-center ">
+    <h1 class="font-bold">Welcome to my personal Website</h1>
+    <div class="bg-gray-200 container mx-auto">
+        <p>My name is Luca and i'm a computer science student.</p>
+        <p>This Website is currently being build and i want to use it to try out a few things. By the time you will see it grow.</p>
+    </div>
+</div>
