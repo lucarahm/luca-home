a220b736988cdde1a30e1eae6cb5ab4f706419fb
Luca Rahm <lucarahm@web.de>
2024-07-01 16:07:11 +0200

    typo

--- svelte-frontend/src/routes/+page.svelte
+++ svelte-frontend/src/routes/+page.svelte
@@ -2,6 +2,6 @@
     <h1 class="font-bold">Welcome to my personal Website</h1>
     <div class="bg-gray-200 container mx-auto">
         <p>My name is Luca and i'm a computer science student.</p>
-        <p>This Website is currently being build and i want to use it to try out a few things. By the time you will see it grow.</p>
+        <p>This Website is currently being built and i want to use it to try out a few things. By the time you will see it grow.</p>
     </div>
 </div>
