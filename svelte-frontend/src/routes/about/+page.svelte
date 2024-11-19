ff3cddacfbdf78985fc5cf8d38f01492be156bee
Luca Rahm <lucarahm@web.de>
2024-11-19 23:02:21 +0100

    small rework of the about page

--- svelte-frontend/src/routes/about/+page.svelte
+++ svelte-frontend/src/routes/about/+page.svelte
@@ -1,9 +1,13 @@
 <article class="prose prose-slate dark:prose-invert">
     <h1>Hi!</h1>
+    <h2 class="font-mono">About me:</h2>
     <p>
-        My name is Luca Rahm, i'm a computer science student. This website is just for fun and I want to implement various things on here over time.
+        My name is Luca, i'm a computer science student. This website is just for fun and I want to implement various things on here over time.
     </p>
-    <h2>Here are some of my planned features:</h2>
+    <p class="">
+        Feel free to poke around <span class="font-extralight text-orange-600">(gently - its still work in progress)</span>.
+    </p>
+    <h2 class="font-mono">Planned features:</h2>
     <ul>
         <li>Add some small games, for example <a href="https://en.wikipedia.org/wiki/2048_(video_game)"> 2048 </a></li>
         <li>Implement a shopping list linked to the user account</li>
