d40e54fa193ec1224e46a8238e1832ccf33faae4
Luca Rahm <lucarahm@web.de>
2024-12-17 20:58:20 +0100

    added shadcn-svelte

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Welcome to <span class="text-xl text-orange-400">Luca Home</span> :) </h1>
+
+<p>Get to the demo <a href="/demo">here</a></p>
